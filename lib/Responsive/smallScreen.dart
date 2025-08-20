b856792ebaabcf15686969e2a45b6c811eb9c6f0
Coders Code <magmaacademy@gmail.com>
2025-08-20 15:32:33 +0500

    v1.1.1 small screen update

--- lib/Responsive/smallScreen.dart
+++ lib/Responsive/smallScreen.dart
@@ -6,12 +6,22 @@ class Smallscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text(
-          'This is a small screen',
-          style: TextStyle(fontSize: 24, color: Colors.red),
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            'Small Screen',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-    );
+        SizedBox(height: 20),
+        Text(
+          'This is a small screen layout.',
+          style: TextStyle(fontSize: 14),
+        ),
+      ],
+    ));
   }
 }
